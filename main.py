eaed3df92380db4b31c2ef5a168693f634af79e1
Charlie Doherty <charlieedoherty@gmail.com>
2024-11-24 19:19:43 -0600

    Create test cases

--- main.py
+++ main.py
@@ -8,12 +8,33 @@ Collaborators: None
 """
 
 
-def dijkstra():
-    pass
+def dijkstra(graph, start, end):
+    return graph, start, end
 
 
 def main():
-    pass
+    graph1 = [[-1, 6, 3, -1],
+              [6, -1, 4, 1],
+              [3, 4, -1, 1],
+              [-1, 1, 1, -1]]
+
+    graph2 = [[-1, 2, -1, 8, -1, -1],
+              [2, -1, -1, 5, 6, -1],
+              [-1, -1, -1, -1, 9, 3],
+              [8, 5, -1, -1, 3, 2],
+              [-1, 6, 9, 3, -1, 1],
+              [-1, -1, 3, 2, 1, -1]]
+
+    graph3 = [[-1, 3, 20, 12, -1, -1],
+              [3, -1, -1, 5, -1, -1],
+              [20, -1, -1, 9, 2, -1],
+              [12, 5, 9, -1, -1, 7],
+              [-1, -1, 2, -1, -1, 8],
+              [-1, -1, -1, 7, 8, -1]]
+
+    print(dijkstra(graph1, "a", "b"))
+    print(dijkstra(graph2, "a", "c"))
+    print(dijkstra(graph3, "a", "e"))
 
 
 main()
